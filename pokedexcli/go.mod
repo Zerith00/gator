4d6eceb9087217443bcb848314960452892eb0aa
Zerith00 <isgo3868@gmail.com>
2024-12-16 16:34:11 -0500

    PokeDexTest

---
+++ pokedexcli/go.mod
@@ -0,0 +1,3 @@
+module github.com/bootdotdev/go-api-gate/courses/projects/pokedexcli
+
+go 1.23.0
---
+++ pokedexcli/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"time"
+
+	"github.com/bootdotdev/go-api-gate/courses/projects/pokedexcli/internal/pokeapi"
+)
+
+func main() {
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
+	cfg := &config{
+		caughtPokemon: map[string]pokeapi.Pokemon{},
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
+}
