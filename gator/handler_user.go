b3863a05bde6f2b58f1d4d7c7708153a3fd8b397
Zerith00 <isgo3868@gmail.com>
2025-01-13 13:20:16 -0500

    CommV5

--- gator/handler_user.go
+++ gator/handler_user.go
@@ -10,36 +10,30 @@ import (
 )
 
 func handlerRegister(s *state, cmd command) error {
-    if len(cmd.Args) != 1 {
-        return fmt.Errorf("usage: %v <name>", cmd.Name)
-    }
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.Name)
+	}
 
-    name := cmd.Args[0]
+	name := cmd.Args[0]
 
-    // Check if a user already exists
-    _, err := s.db.GetUser(context.Background(), name)
-    if err == nil {
-        return fmt.Errorf("user with name %s already exists", name)
-    }
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
 
-    user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
-        ID:        uuid.New(),
-        CreatedAt: time.Now().UTC(),
-        UpdatedAt: time.Now().UTC(),
-        Name:      name,
-    })
-    if err != nil {
-        return fmt.Errorf("couldn't create user: %w", err)
-    }
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
 
-    err = s.cfg.SetUser(user.Name)
-    if err != nil {
-        return fmt.Errorf("couldn't set current user: %w", err)
-    }
-
-    fmt.Println("User created successfully:")
-    printUser(user)
-    return nil
+	fmt.Println("User created successfully:")
+	printUser(user)
+	return nil
 }
 
 func handlerLogin(s *state, cmd command) error {
@@ -62,26 +56,22 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't list users: %w", err)
+	}
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %v (current)\n", user.Name)
+			continue
+		}
+		fmt.Printf("* %v\n", user.Name)
+	}
+	return nil
+}
+
 func printUser(user database.User) {
 	fmt.Printf(" * ID:      %v\n", user.ID)
 	fmt.Printf(" * Name:    %v\n", user.Name)
-}
-
-func handlerUsers(s *state, _ command) error {
-    users, err := s.db.GetUsers(context.Background())
-    if err != nil {
-        return fmt.Errorf("couldn't get users: %w", err)
-    }
-
-    currentUser := s.cfg.CurrentUserName
-
-    for _, user := range users {
-        if user.Name == currentUser {
-            fmt.Printf("* %s (current)\n", user.Name)
-        } else {
-            fmt.Printf("* %s\n", user.Name)
-        }
-    }
-
-    return nil
 }
